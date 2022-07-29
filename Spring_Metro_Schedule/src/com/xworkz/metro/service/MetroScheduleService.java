70f6cb8244159de5c187f1c46cebe0af0c7f39cd
Nikhil11423 <103095257+Nikhil11423@users.noreply.github.com>
2022-07-29 19:01:26 +0530

    Delete Spring_Metro_Schedule directory

--- Spring_Metro_Schedule/src/com/xworkz/metro/service/MetroScheduleService.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.metro.service;
-
-import com.xworkz.metro.dto.MetroScheduleDTO;
-
-public interface MetroScheduleService {
-	
-	boolean validateAndSave(MetroScheduleDTO dto);
-
-}
