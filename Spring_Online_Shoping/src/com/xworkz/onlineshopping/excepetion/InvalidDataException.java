ebfffce494dd7113aa034bb98f5a6853c26084d0
Nikhil11423 <103095257+Nikhil11423@users.noreply.github.com>
2022-07-29 18:59:53 +0530

    Delete Spring_Online_Shoping directory

--- Spring_Online_Shoping/src/com/xworkz/onlineshopping/excepetion/InvalidDataException.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.onlineshopping.excepetion;
-
-public class InvalidDataException extends Exception {
-
-	public InvalidDataException(String msg) {
-		super(msg);
-	}
-
-}
--- Spring_Online_Shoping/src/com/xworkz/onlineshopping/service/OnlineShoppingService.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.onlineshopping.service;
-
-import com.xworkz.onlineshopping.dto.OnlineShoppingDTO;
-import com.xworkz.onlineshopping.excepetion.InvalidDataException;
-
-public interface OnlineShoppingService {
-
-	boolean validateAndSave(OnlineShoppingDTO shoppingDTO)throws InvalidDataException;
-}
