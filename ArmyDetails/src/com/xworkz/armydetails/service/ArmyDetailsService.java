f3ef571591ae1589038c7ca16e4f3049abb3b3d8
Nikhil11423 <103095257+Nikhil11423@users.noreply.github.com>
2022-07-29 18:58:37 +0530

    Delete ArmyDetails directory

--- ArmyDetails/src/com/xworkz/armydetails/service/ArmyDetailsService.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.armydetails.service;
-
-import com.xworkz.armydetail.excepetion.InvalidDataException;
-import com.xworkz.armydetails.dto.ArmyDetailsDTO;
-
-public interface ArmyDetailsService {
-
-	boolean validateAndSave(ArmyDetailsDTO dto)throws InvalidDataException ;
-}
