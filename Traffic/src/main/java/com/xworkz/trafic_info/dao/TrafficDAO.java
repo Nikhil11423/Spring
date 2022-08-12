54bf742bcc640b07f6d526ed86983594812b6d7b
Nikhil11423 <103095257+Nikhil11423@users.noreply.github.com>
2022-08-12 10:43:01 +0530

    Delete Traffic directory

--- Traffic/src/main/java/com/xworkz/trafic_info/dao/TrafficDAO.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.trafic_info.dao;
-
-import com.xworkz.trafic_info.dto.TrafficDTO;
-
-public interface TrafficDAO {
-	
-	boolean save(TrafficDTO dto);
-
-}
--- Traffic/src/main/java/com/xworkz/trafic_info/service/TrafficService.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.trafic_info.service;
-
-import com.xworkz.trafic_info.dto.TrafficDTO;
-
-public interface TrafficService {
-	
-	boolean validateAndSave(TrafficDTO dto);
-
-}
