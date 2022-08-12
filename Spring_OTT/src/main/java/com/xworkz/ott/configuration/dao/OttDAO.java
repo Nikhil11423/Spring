d24e90c404de0de33aa39103d3e1c3c7648fdb95
Nikhil11423 <103095257+Nikhil11423@users.noreply.github.com>
2022-08-12 10:40:29 +0530

    Delete Spring_OTT directory

--- Spring_OTT/src/main/java/com/xworkz/ott/configuration/dao/OttDAO.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.ott.configuration.dao;
-
-import com.xworkz.ott.dto.OttDTO;
-
-public interface OttDAO {
-	
-	boolean save(OttDTO dto);
-}
--- Spring_OTT/src/main/java/com/xworkz/ott/configuration/service/OttService.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.ott.configuration.service;
-
-import com.xworkz.ott.dto.OttDTO;
-
-public interface OttService {
-	
-	boolean validateAndSave(OttDTO dto);
-
-}
